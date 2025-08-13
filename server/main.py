7ea89b1c4495dfa806e3be17f9c514e5498839d3
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2025-08-13 21:39:03 +0530

    Add authentication routes and include router in main application
    Signed-off-by: Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>

--- server/main.py
+++ server/main.py
@@ -2,14 +2,13 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, Query # type: ignore
 from sqlmodel import Field, Session, SQLModel, create_engine, select # type: ignore
-
+from app.api import routes_auth
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
-
-# Code below omitted 👇
+ 
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -24,7 +23,7 @@ SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
 
-app.include_router()
+app.include_router(routes_auth.router)
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
